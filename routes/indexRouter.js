264ba4aa41edc0b9dc8894d1347c07016179bd2d
Dilpreet Singh <dilpreetsinghaulakh@icloud.com>
2025-06-13 16:11:18 +0530

    feat: move message posting route to newRouter for better organization

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -4,6 +4,5 @@ const indexRouter = Router();
 const messageController = require("../controllers/messagesController");
 
 indexRouter.get("/", messageController.home);
-indexRouter.post("/new", messageController.addNewMessage);
 
 module.exports = indexRouter;
--- routes/newRouter.js
+++ routes/newRouter.js
@@ -1,8 +1,11 @@
 const { Router } = require("express");
 const newRouter = Router();
 
+const messageController = require("../controllers/messagesController");
+
 newRouter.get("/", (req, res) => {
   res.render("form");
 });
+newRouter.post("/", messageController.addNewMessage);
 
 module.exports = newRouter;
